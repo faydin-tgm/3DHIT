7bea6e4a381b3971c1c207dafabe5cfe46391317
Faydin <faydin@student.tgm.ac.at>
2017-02-23 21:52:54 +0100

    change5

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,15 @@
+/**
+ * Die HelloWorld Klasse hat einen Konstruktor, in der es "Hello World!" auf der Konsole ausgibt.
+ * 
+ * @author Fatih Aydin
+ * @version 23.02.2017
+ */
 
 public class HelloWorld {
+	
+	/**
+	 * Konstruktor
+	 */
 	public HelloWorld(){
 		System.out.print("Hello World!"); 
 	}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+/**
+ * Die Main Klasse besitzt eine main-Methode und ruft die Klasse HelloWorld auf.
+ * 
+ * @author Fatih Aydin
+ * @version 23.02.2017
+ */
 
 public class Main {
+	
+	/**
+	 * Main Methode
+	 * @param args
+	 */
 	public static void main(String[] args){
 		new HelloWorld(); 
 	}
